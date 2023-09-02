2acc923b2b18ba58771cae7d1577ecc000544113
laianelimac <laianelima@hotmail.com>
2023-09-02 20:54:52 -0300

    atualização

---
+++ jj/Aula3ativ3.py
@@ -0,0 +1,9 @@
+# Faça um programa que capture o nome do usuário, altura em metros, idade e imprima esses dados na tela. #
+
+nome = input(" Por favor, insira seu nome: ")
+altura = float(input(" Por favor, insira sua altura: "))
+idade = int(input(" Por favor, insira sua idade: "))
+
+mensagem = f"Olá, meu nome é {nome} , tenho {altura} de altura e tenho {idade} anos."
+
+print(mensagem)
