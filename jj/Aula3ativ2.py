54e76e3230045000b4a307b204d382b9a8d1e428
laianelimac <laianelima@hotmail.com>
2023-09-02 20:34:27 -0300

    atualização

---
+++ jj/Aula3ativ2.py
@@ -0,0 +1,10 @@
+"""Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 
+e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10."""
+
+nome = "PAULA MARTINS"
+mes = "JANEIRO"
+valor = "500,00"
+desconto = "10%"
+cupom = "PAULAÉ10"
+
+print('Olá, ' + nome + '. Em ' + mes + ' você realizou uma compra no valor de ' + valor + '  ganhou um desconto de ' + desconto + ' em sua próxima compra. Use o cupom ' + cupom + '.')
