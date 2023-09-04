f0782a5ccb3e3328b87a335e44e2cdc758838b81
laianelimac <laianelima@hotmail.com>
2023-09-04 18:47:33 -0300

    atualização

---
+++ jj/Aula4ativ1.py
@@ -0,0 +1,13 @@
+numero = float(input('Escolha um número:'))
+
+calculo1 = numero * 2 
+calculo2 = numero * 3
+calculo3 = numero * numero
+calculo4 = numero * 1/2
+calculo5 = numero * 1/3
+
+print(f'O dobro do número inserido é: {calculo1}')
+print(f'O triplo do número inserido é: {calculo2}')
+print(f'O quadrado do número inserido é: {calculo3}')
+print(f'A metade do número inserido é: {calculo4}')
+print(f'A terça parte do número inserido é: {calculo5}')
