b214437982b05d55e069f76ab16023bdcf0a636d
laianelimac <laianelima@hotmail.com>
2023-09-04 18:26:15 -0300

    atualização

---
+++ jj/Aula4ativ2.py
@@ -0,0 +1,9 @@
+nome = input('Qual é o seu nome?')
+n1 = int(input('Digite sua primeira nota: '))
+n2 = int(input('Digite sua segunda nota: '))
+n3 = int(input('Digite sua terceira nota: '))
+n4 = int(input('Digite sua quarta nota: '))
+
+mediaTotal = (n1+n2+n3+n4)/4
+
+print(f'Olá, {nome}! Sua média é : {mediaTotal}.')
