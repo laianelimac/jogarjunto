d5e51bf016ab48e409b1c3f2e02ded4926b98560
laianelimac <laianelima@hotmail.com>
2023-09-08 15:34:24 -0300

    atualização atividade 5

---
+++ jj/Aula5ativ3.py
@@ -0,0 +1,5 @@
+diasTreino = int(input('Quantos dias você treinou sem interrupção?'))
+if diasTreino >= 21:
+    print('UHUU. AGORA VOCÊ PODE PRESENTEAR UM AMIGO OU AMIGA PARA TREINAR COM VOCÊ')
+else:
+    print('QUE BOM VER VOCÊ DE VOLTA. A PARTIR DE AGORA INICIAMOS MAIS UMA CONTAGEM DE 21 DIAS PARA A PROMO TREINA JUNTO.')   
