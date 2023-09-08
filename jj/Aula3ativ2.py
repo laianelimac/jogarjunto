6300a77c1d0d0e29231f73899059705e51a6f60b
laianelimac <laianelima@hotmail.com>
2023-09-08 15:11:45 -0300

    atualização atividade 5

--- jj/Aula3ativ2.py
+++ jj/Aula3ativ2.py
@@ -1,5 +1,5 @@
-"""Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 
-e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10."""
+#Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 
+#e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
 
 nome = "PAULA MARTINS"
 mes = "JANEIRO"
